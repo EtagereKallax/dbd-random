1e2cf2bc21ab067244c09050681d8e8f7eb0f967
EtagereKallax <78915201+EtagereKallax@users.noreply.github.com>
2024-04-10 15:30:11 +0200

    Add files via upload

---
+++ script/randomizer-constants.js
@@ -0,0 +1,10 @@
+const SLOT_MACHINE_DELAY = 0;
+const DELAY_BEFORE_CHARACTER_ROLE = 0;
+const SHOW_ROLE_RESULT_TIME = 0;
+const ROLE_SLOT_SHUFFLE_TIME = 0; // the duration of the role shuffle in ms
+const PERK_SLOT_SHUFFLE_TIME = 0; // the duration of the perk shuffle in ms
+const CHARACTER_SLOT_SHUFFLE_TIME = 0; // the duration of the character shuffle in ms
+const ITEMS_SLOT_SHUFFLE_TIME = 0; // the duration of the items shuffle in ms
+const ADDONS_SLOT_SHUFFLE_TIME = 0; // the duration of the addons shuffle in ms
+const OFFERING_SLOT_SHUFFLE_TIME = 0; // the duration of the offerings shuffle in ms
+const AUDIO_VOLUME = 0.5; // audio volume from 0 to 1.
